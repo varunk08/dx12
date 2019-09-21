42d03e75d7a636ece5400a50b111853203c97f15
varun <varunk08@users.noreply.github.com>
2019-09-21 13:15:52 -0400

    texture demo. begin. doesn't build

--- projects/texturing/texturing.cpp
+++ projects/texturing/texturing.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 
+#include "windows.h"
+
+#include "BaseApp.h"
+
 using namespace std;
 
-int main(int argc, char** argv)
+// ====================================================================================================================
+class TextureDemo : public BaseApp
 {
-		cout << "Hello texturing!" << endl;
-		return 0;
+public:
+private:
 }
+
+// ====================================================================================================================
+// Write the win main func here
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   PSTR      pCmdLine,
+                   int       nShowCmd)
+{
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    TextureDemo demoApp(hInstance);
+
+    int retCode = 0;
+    if (demoApp.Initialize() == true)
+    {
+        retCode = demoApp.Run();
+    }
+
+    return retCode;
+}
+
+
+
