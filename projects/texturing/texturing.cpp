def5fe9e439bba9b72ba0c6679e26b776fe05a33
varun <varunk08@users.noreply.github.com>
2019-09-22 17:53:40 -0400

    texturing. fix build.

--- projects/texturing/texturing.cpp
+++ projects/texturing/texturing.cpp
@@ -10,9 +10,42 @@ using namespace std;
 class TextureDemo : public BaseApp
 {
 public:
+    TextureDemo(HINSTANCE hInstance)
+        :
+        BaseApp(hInstance)
+    {}
+    virtual void Update(const BaseTimer& timer) override;
+    virtual void Draw(const BaseTimer& timer) override;
+
+    bool Initialize() override;
 private:
+};
+
+// ====================================================================================================================
+void TextureDemo::Update(const BaseTimer& timer)
+{
+
 }
 
+// ====================================================================================================================
+void TextureDemo::Draw(const BaseTimer& timer)
+{
+
+}
+
+// ====================================================================================================================
+bool TextureDemo::Initialize()
+{
+    bool result = true;
+
+    result = BaseApp::Initialize();
+    if (result == true)
+    {
+        
+    }
+
+    return result;
+}
 // ====================================================================================================================
 // Write the win main func here
 int WINAPI WinMain(HINSTANCE hInstance,
