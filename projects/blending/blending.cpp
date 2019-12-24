2bd4b6a6e6d25b511607ce6da73adb8f3fe55c5a
varunk08 <varunk08@users.noreply.github.com>
2019-12-24 07:45:56 -0500

    blending new project

---
+++ projects/blending/blending.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include "DirectXColors.h"
+#include "windows.h"
+#include "BaseApp.h"
+
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   PSTR      pCmdLine,
+                   int       nShowCmd)
+{
+  cout << "Hello, blending" << endl;
+  return 0;
+}
