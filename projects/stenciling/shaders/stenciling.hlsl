9e7106800df3202c63173bc9d2120365fe08827f
varun <varunk08@users.noreply.github.com>
2020-01-26 13:04:23 -0500

    clear screen draw commands added

---
+++ projects/stenciling/shaders/stenciling.hlsl
@@ -0,0 +1,16 @@
+struct VertexIn
+{
+};
+
+struct VertexOut
+{
+};
+
+VertexOut VS(VertexIn vin)
+{
+};
+
+
+float4 PS(VertexOut pin)
+{
+};
--- projects/stenciling/stenciling.cpp
+++ projects/stenciling/stenciling.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include "DirectXColors.h"
 #include "BaseApp.h"
 
 using namespace std;
+using namespace DirectX;
 
 // CBs that the shaders need.
 struct PassCb
@@ -47,12 +49,66 @@ public:
   }
   
   virtual void OnResize()override
-  {}
+  {
+      BaseApp::OnResize();
+  }
   
-  virtual void Update(const BaseTimer& gt)override{}
+  virtual void Update(const BaseTimer& gt)override
+  {
+      if ((m_currentFence != 0) && (m_fence->GetCompletedValue() < m_currentFence)) {
+          HANDLE eventHandle = CreateEventEx(nullptr, false, false, EVENT_ALL_ACCESS);
+          ThrowIfFailed(m_fence->SetEventOnCompletion(m_currentFence, eventHandle));
+          WaitForSingleObject(eventHandle, INFINITE);
+          CloseHandle(eventHandle);
+      }
+  }
 
   virtual void Draw(const BaseTimer& gt)override
   {
+      ThrowIfFailed(m_directCmdListAlloc->Reset());
+      ThrowIfFailed(m_commandList->Reset(m_directCmdListAlloc.Get(), nullptr));
+
+      m_commandList->RSSetViewports(1, &m_screenViewport);
+      m_commandList->RSSetScissorRects(1, &m_scissorRect);
+
+      // Transition the back buffer so we can render to it.
+      m_commandList->ResourceBarrier(1,
+        &CD3DX12_RESOURCE_BARRIER::Transition(
+            CurrentBackBuffer(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+
+      m_commandList->ClearRenderTargetView(CurrentBackBufferView(),
+                                           DirectX::Colors::Gray,
+                                           0,
+                                           nullptr);
+
+      m_commandList->ClearDepthStencilView(DepthStencilView(),
+                                           D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL,
+                                           1.0f,
+                                           0,
+                                           0,
+                                           nullptr);
+      m_commandList->OMSetRenderTargets(1,                        // Num render target descriptors
+                                        &CurrentBackBufferView(), // handle to rt
+                                        true,                     // descriptors are contiguous
+                                        &DepthStencilView());     // handle to ds
+
+      // Transition the render target back to be presentable.
+      m_commandList->ResourceBarrier(1,
+          &CD3DX12_RESOURCE_BARRIER::Transition(
+            CurrentBackBuffer(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+      ThrowIfFailed(m_commandList->Close());
+      ID3D12CommandList* command_lists[] = { m_commandList.Get() };
+      m_commandQueue->ExecuteCommandLists(_countof(command_lists), command_lists);
+
+      // Present current swap chain buffer and advance to next buffer.
+      ThrowIfFailed(m_swapChain->Present(0, 0));
+      m_currBackBuffer = (m_currBackBuffer + 1) % SwapChainBufferCount;
+
+      ++m_currentFence;
+      m_commandQueue->Signal(m_fence.Get(), m_currentFence);
+
   }
 
   virtual void OnMouseDown(WPARAM btnState, int x, int y)override{}
@@ -62,21 +118,26 @@ public:
   void LoadTextures()
   {
   }
-  void BuildGeometry()
-  {
 
+  void BuildSceneGeometry()
+  {
+      
+      
   }
 
   void BuildRenderItems()
   {
   }
+
   void BuildMaterials()
   {
   }
+
   void BuildShadersAndInputLayout()
   {
   }
-  
+
+  // Builds shaders, descriptors and pipelines. 
   void BuildPipelines()
   {
     BuildShadersAndInputLayout();
@@ -86,13 +147,25 @@ public:
   {
     // Initializes main window and d3d.
     bool result = BaseApp::Initialize();
-
+    
     if (result == true) {
-      LoadTextures();
-      BuildGeometry();
-      BuildMaterials();
-      BuildPipelines();
-      BuildRenderItems();
+        ThrowIfFailed(m_commandList.Get()->Reset(m_directCmdListAlloc.Get(), nullptr));
+
+        LoadTextures();
+        BuildSceneGeometry();
+        BuildMaterials();
+        BuildPipelines();
+        BuildRenderItems();
+
+        // After writing the commands needed to build resources, which involves uploading
+        // some of them to GPU memory, submit the commands.
+        ThrowIfFailed(m_commandList->Close());
+        ID3D12CommandList* cmdLists[] = { m_commandList.Get() };
+        m_commandQueue->ExecuteCommandLists(_countof(cmdLists), cmdLists);
+        FlushCommandQueue(); // Queue-submit.
+    }
+    else {
+        ::OutputDebugStringA("Error initializing stencil demo\n");
     }
     
     return result;
@@ -134,11 +207,13 @@ TODO:
 - Write WinMain
 - Demo class
 - Initialize function
+
+- Clear the screen with some color.
 - Load basic 3D geometry
 - Camera controls
 - Implement lighting
 - Implement texturing
 - Implement shadows
 - Implement stenciling mirror.
-
+- Move common types to vkCommon.
  ***********************************************************************/
