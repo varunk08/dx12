145cce16c6152bb32feb455fecb089c9ce92ba03
varun <varunk08@users.noreply.github.com>
2020-01-28 22:33:04 -0500

    static samplers clean up

--- projects/stenciling/stenciling.cpp
+++ projects/stenciling/stenciling.cpp
@@ -541,62 +541,6 @@ public:
                                                            IID_PPV_ARGS(&m_pipelines["opaque"])));
   }
 
-  std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> GetStaticSamplers()
-  {
-      // Applications usually only need a handful of samplers.  So just define them all up front
-      // and keep them available as part of the root signature.
-
-      const CD3DX12_STATIC_SAMPLER_DESC pointWrap(
-          0, // shaderRegister
-          D3D12_FILTER_MIN_MAG_MIP_POINT, // filter
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP); // addressW
-
-      const CD3DX12_STATIC_SAMPLER_DESC pointClamp(
-          1, // shaderRegister
-          D3D12_FILTER_MIN_MAG_MIP_POINT, // filter
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP); // addressW
-
-      const CD3DX12_STATIC_SAMPLER_DESC linearWrap(
-          2, // shaderRegister
-          D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP); // addressW
-
-      const CD3DX12_STATIC_SAMPLER_DESC linearClamp(
-          3, // shaderRegister
-          D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP); // addressW
-
-      const CD3DX12_STATIC_SAMPLER_DESC anisotropicWrap(
-          4, // shaderRegister
-          D3D12_FILTER_ANISOTROPIC, // filter
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
-          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressW
-          0.0f,                             // mipLODBias
-          8);                               // maxAnisotropy
-
-      const CD3DX12_STATIC_SAMPLER_DESC anisotropicClamp(
-          5, // shaderRegister
-          D3D12_FILTER_ANISOTROPIC, // filter
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
-          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressW
-          0.0f,                              // mipLODBias
-          8);                                // maxAnisotropy
-
-      return {
-          pointWrap, pointClamp,
-          linearWrap, linearClamp,
-          anisotropicWrap, anisotropicClamp };
-  }
 
   virtual bool Initialize() override
   {
@@ -647,6 +591,58 @@ public:
   POINT                                                   m_lastMousePos;
   ComPtr<ID3D12DescriptorHeap>                            m_srvDescriptorHeap = nullptr;
 
+
+  const std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6>  m_staticSamplers =
+  {
+      // Applications usually only need a handful of samplers.  So just define them all up front
+      // and keep them available as part of the root signature.
+
+      CD3DX12_STATIC_SAMPLER_DESC(// pointWrap
+          0, // shaderRegister
+          D3D12_FILTER_MIN_MAG_MIP_POINT, // filter
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP), // addressW
+
+      CD3DX12_STATIC_SAMPLER_DESC( // pointClamp
+          1, // shaderRegister
+          D3D12_FILTER_MIN_MAG_MIP_POINT, // filter
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP), // addressW
+
+      CD3DX12_STATIC_SAMPLER_DESC( // linearWrap
+          2, // shaderRegister
+          D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP), // addressW
+
+      CD3DX12_STATIC_SAMPLER_DESC( // linearClamp
+          3, // shaderRegister
+          D3D12_FILTER_MIN_MAG_MIP_LINEAR, // filter
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP), // addressW
+
+      CD3DX12_STATIC_SAMPLER_DESC( // anisotropicWrap
+          4, // shaderRegister
+          D3D12_FILTER_ANISOTROPIC, // filter
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressU
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressV
+          D3D12_TEXTURE_ADDRESS_MODE_WRAP,  // addressW
+          0.0f,                             // mipLODBias
+          8),                               // maxAnisotropy
+
+      CD3DX12_STATIC_SAMPLER_DESC( // anisotropicClamp
+          5, // shaderRegister
+          D3D12_FILTER_ANISOTROPIC, // filter
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressU
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressV
+          D3D12_TEXTURE_ADDRESS_MODE_CLAMP,  // addressW
+          0.0f,                              // mipLODBias
+          8),                                // maxAnisotropy
+  };
 
 }; // Class StencilDemo
 
