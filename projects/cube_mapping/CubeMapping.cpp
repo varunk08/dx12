580816abc008d62d792149ac9ab9a1d10f954f78
varunk08 <varunk08@users.noreply.github.com>
2020-08-21 08:51:51 -0400

    renders scene using two pipelines, scene also has two submeshes

--- projects/cube_mapping/CubeMapping.cpp
+++ projects/cube_mapping/CubeMapping.cpp
@@ -1,6 +1,6 @@
 /*
 * TODO:
-* - create skybox pipeline
+* + create skybox pipeline
 * - create skybox geometry (sphere)
 * - add shader for skybox rendering using cube map
 * - draw skybox geometry
@@ -43,17 +43,17 @@ public:
     ~Camera() {
     }
     void SetPosition(float x, float y, float z) {
-        mPosition  = XMFLOAT3(x, y, z);
+        mPosition = XMFLOAT3(x, y, z);
         mViewDirty = true;
     }
     void SetLens(float fovY, float aspect, float zn, float zf) {
-        mFovY             = fovY;
-        mAspect           = aspect;
-        mNearZ            = zn;
-        mFarZ             = zf;
-        mNearWindowHeight = 2.0f * mNearZ * tanf(0.5f*mFovY);
-        mFarWindowHeight  = 2.0f * mFarZ * tanf(0.5f*mFovY);
-        XMMATRIX P        = XMMatrixPerspectiveFovLH(mFovY, mAspect, mNearZ, mFarZ);
+        mFovY = fovY;
+        mAspect = aspect;
+        mNearZ = zn;
+        mFarZ = zf;
+        mNearWindowHeight = 2.0f * mNearZ * tanf(0.5f * mFovY);
+        mFarWindowHeight = 2.0f * mFarZ * tanf(0.5f * mFovY);
+        XMMATRIX P = XMMatrixPerspectiveFovLH(mFovY, mAspect, mNearZ, mFarZ);
         XMStoreFloat4x4(&mProj, P);
     }
     XMMATRIX GetView() const {
@@ -132,17 +132,17 @@ public:
 private:
     // Camera coordinate system with coordinates relative to world space.
     DirectX::XMFLOAT3 mPosition = { 0.0f, 0.0f, 0.0f };
-    DirectX::XMFLOAT3 mRight    = { 1.0f, 0.0f, 0.0f };
-    DirectX::XMFLOAT3 mUp       = { 0.0f, 1.0f, 0.0f };
-    DirectX::XMFLOAT3 mLook     = { 0.0f, 0.0f, 1.0f };
+    DirectX::XMFLOAT3 mRight = { 1.0f, 0.0f, 0.0f };
+    DirectX::XMFLOAT3 mUp = { 0.0f, 1.0f, 0.0f };
+    DirectX::XMFLOAT3 mLook = { 0.0f, 0.0f, 1.0f };
     // Cache frustum properties.
-    float mNearZ            = 0.0f;
-    float mFarZ             = 0.0f;
-    float mAspect           = 0.0f;
-    float mFovY             = 0.0f;
+    float mNearZ = 0.0f;
+    float mFarZ = 0.0f;
+    float mAspect = 0.0f;
+    float mFovY = 0.0f;
     float mNearWindowHeight = 0.0f;
-    float mFarWindowHeight  = 0.0f;
-    bool mViewDirty         = true;
+    float mFarWindowHeight = 0.0f;
+    bool mViewDirty = true;
     // Cache View/Proj matrices.
     DirectX::XMFLOAT4X4 mView = MathHelper::Identity4x4();
     DirectX::XMFLOAT4X4 mProj = MathHelper::Identity4x4();
@@ -153,7 +153,7 @@ class CubeMapDemo : public BaseApp
 {
 public:
     CubeMapDemo(HINSTANCE hInst)
-    : BaseApp(hInst){
+        : BaseApp(hInst) {
     }
     ~CubeMapDemo() {
         if (m_d3dDevice != nullptr) {
@@ -186,30 +186,30 @@ public:
 protected:
     void LoadTextures() {
         std::wstring cubeMapImgFilename = L"..\\..\\..\\projects\\Textures\\sunsetCube1024.dds";
-        auto texMap       = std::make_unique<Texture>();
-        texMap->name_     = "cubeMap";
+        auto texMap = std::make_unique<Texture>();
+        texMap->name_ = "cubeMap";
         texMap->filename_ = cubeMapImgFilename;
         ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(m_d3dDevice.Get(),
-                                                          m_commandList.Get(),
-                                                          texMap->filename_.c_str(),
-                                                          texMap->resource_,
-                                                          texMap->uploadHeap_));
+            m_commandList.Get(),
+            texMap->filename_.c_str(),
+            texMap->resource_,
+            texMap->uploadHeap_));
         mCubeMapTex = std::move(texMap);
     }
 
     void OnKeyboardInput(const BaseTimer& gt) {
         const float dt = gt.DeltaTimeInSecs();
         if (GetAsyncKeyState('W') & 0x8000) {
-            mCamera.Walk(10.0f*dt);
+            mCamera.Walk(10.0f * dt);
         }
         if (GetAsyncKeyState('S') & 0x8000) {
-            mCamera.Walk(-10.0f*dt);
+            mCamera.Walk(-10.0f * dt);
         }
         if (GetAsyncKeyState('A') & 0x8000) {
-            mCamera.Strafe(-10.0f*dt);
+            mCamera.Strafe(-10.0f * dt);
         }
         if (GetAsyncKeyState('D') & 0x8000) {
-            mCamera.Strafe(10.0f*dt);
+            mCamera.Strafe(10.0f * dt);
         }
         mCamera.UpdateViewMatrix();
     }
@@ -240,22 +240,29 @@ protected:
         m_commandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSlateGray, 0, nullptr);
         m_commandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
         m_commandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
-        m_commandList->IASetVertexBuffers(0, 1, &mGeometries["grid"]->VertexBufferView());
-        m_commandList->IASetIndexBuffer(&mGeometries["grid"]->IndexBufferView());
+        m_commandList->IASetVertexBuffers(0, 1, &mGeometries["all"]->VertexBufferView());
+        m_commandList->IASetIndexBuffer(&mGeometries["all"]->IndexBufferView());
         m_commandList->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
         m_commandList->SetGraphicsRootSignature(mRootSignature.Get());
 
         // Only one heap of any type can be set at any time apparently. So we can set a max of two heap types - CBV/SRV/UAV and Samplers.
-        ID3D12DescriptorHeap* descriptorHeaps[] = { mpDescHeap.Get()};
+        ID3D12DescriptorHeap* descriptorHeaps[] = { mpDescHeap.Get() };
         m_commandList->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
-        
+
         m_commandList->SetGraphicsRootDescriptorTable(0, mpDescHeap->GetGPUDescriptorHandleForHeapStart());
-        
+
         CD3DX12_GPU_DESCRIPTOR_HANDLE skyTexDescriptor(mpDescHeap->GetGPUDescriptorHandleForHeapStart());
         skyTexDescriptor.Offset(1, m_cbvSrvUavDescriptorSize);
         m_commandList->SetGraphicsRootDescriptorTable(1, skyTexDescriptor);
 
-        m_commandList->DrawIndexedInstanced(mGeometries["grid"]->drawArgs["grid"].indexCount, 1, 0, 0, 0);
+        m_commandList->DrawIndexedInstanced(mGeometries["all"]->drawArgs["grid"].indexCount, 1, 0, 0, 0);
+        
+        m_commandList->SetPipelineState(mSkyRenderPipeline.Get());
+        m_commandList->DrawIndexedInstanced(mGeometries["all"]->drawArgs["sphere"].indexCount,
+                                            1,
+                                            mGeometries["all"]->drawArgs["sphere"].startIndexLocation,
+                                            mGeometries["all"]->drawArgs["sphere"].baseVertexLocation,
+                                            0);
         m_commandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
         ThrowIfFailed(m_commandList->Close());
         ID3D12CommandList* cmdsLists[] = { m_commandList.Get() };
@@ -289,7 +296,7 @@ protected:
     }
     void OnResize() {
         BaseApp::OnResize();
-        mCamera.SetLens(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.f);
+        mCamera.SetLens(0.25f * MathHelper::Pi, AspectRatio(), 1.0f, 1000.f);
         /*XMMATRIX proj = XMMatrixPerspectiveFovLH(0.25f * MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
         XMStoreFloat4x4(&mProj, proj);*/
     }
@@ -322,6 +329,29 @@ protected:
         psoDesc.DSVFormat = m_depthStencilFormat;
         HRESULT hr = m_d3dDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mSimplePipeline));
         ThrowIfFailed(hr);
+
+        D3D12_GRAPHICS_PIPELINE_STATE_DESC skyPsoDesc = psoDesc;
+
+        // The camera is inside the sky sphere, so just turn off culling.
+        skyPsoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
+
+        // Make sure the depth function is LESS_EQUAL and not just LESS.  
+        // Otherwise, the normalized depth values at z = 1 (NDC) will 
+        // fail the depth test if the depth buffer was cleared to 1.
+        skyPsoDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+        skyPsoDesc.pRootSignature              = mRootSignature.Get();
+        skyPsoDesc.VS =
+        {
+            reinterpret_cast<BYTE*>(mShaders["skyVS"]->GetBufferPointer()),
+            mShaders["skyVS"]->GetBufferSize()
+        };
+        skyPsoDesc.PS =
+        {
+            reinterpret_cast<BYTE*>(mShaders["skyPS"]->GetBufferPointer()),
+            mShaders["skyPS"]->GetBufferSize()
+        };
+        ThrowIfFailed(m_d3dDevice->CreateGraphicsPipelineState(&skyPsoDesc, IID_PPV_ARGS(&mSkyRenderPipeline)));
+
     }
     std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> GetStaticSamplers()
     {
@@ -380,10 +410,10 @@ protected:
             anisotropicWrap, anisotropicClamp };
     }
     void BuildRootSignature() {
-     
+
         CD3DX12_DESCRIPTOR_RANGE cbvTable = {};
         cbvTable.Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 1, 0);
-        
+
         CD3DX12_DESCRIPTOR_RANGE texTable = {};
         texTable.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0);
 
@@ -425,15 +455,18 @@ protected:
         hDescriptor.Offset(1, m_cbvSrvUavDescriptorSize);
         auto cubeMapRes = mCubeMapTex->resource_;
         D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
-        srvDesc.Shader4ComponentMapping         = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-        srvDesc.Format                          = cubeMapRes->GetDesc().Format;
-        srvDesc.ViewDimension                   = D3D12_SRV_DIMENSION_TEXTURECUBE;
-        srvDesc.TextureCube.MostDetailedMip     = 0;
-        srvDesc.TextureCube.MipLevels           = cubeMapRes->GetDesc().MipLevels;
+        srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+        srvDesc.Format = cubeMapRes->GetDesc().Format;
+        srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURECUBE;
+        srvDesc.TextureCube.MostDetailedMip = 0;
+        srvDesc.TextureCube.MipLevels = cubeMapRes->GetDesc().MipLevels;
         srvDesc.TextureCube.ResourceMinLODClamp = 0.0f;
         m_d3dDevice->CreateShaderResourceView(cubeMapRes.Get(), &srvDesc, hDescriptor);
     }
     void BuildShaders() {
+        mShaders["skyVS"] = BaseUtil::CompileShader(L"..\\..\\..\\projects\\cube_mapping\\shaders\\simpleRenderSky.hlsl", nullptr, "SimpleVS", "vs_5_1");
+        mShaders["skyPS"] = BaseUtil::CompileShader(L"..\\..\\..\\projects\\cube_mapping\\shaders\\simpleRenderSky.hlsl", nullptr, "SimplePS", "ps_5_1");
+
         mShaders["simpleVS"] = BaseUtil::CompileShader(L"..\\..\\..\\projects\\cube_mapping\\shaders\\simpleRender.hlsl", nullptr, "SimpleVS", "vs_5_1");
         mShaders["simplePS"] = BaseUtil::CompileShader(L"..\\..\\..\\projects\\cube_mapping\\shaders\\simpleRender.hlsl", nullptr, "SimplePS", "ps_5_1");
         mInputLayout = {
@@ -444,35 +477,48 @@ protected:
     void BuildGeometry() {
         GeometryGenerator generator;
         MeshData grid                  = generator.CreateGrid(20.0f, 20.0f, 40, 40);
+        MeshData sphere                = generator.CreateSphere(5.0f, 20, 20);
         UINT gridVertexOffset          = 0;
         UINT gridIndexOffset           = 0;
         SubmeshGeometry gridSubmesh    = {};
         gridSubmesh.indexCount         = static_cast<UINT>(grid.m_indices32.size());
         gridSubmesh.startIndexLocation = gridIndexOffset;
         gridSubmesh.baseVertexLocation = gridVertexOffset;
-        vector<ShaderVertex> vertices(grid.m_vertices.size());
+
+        SubmeshGeometry sphereSubmesh    = {};
+        sphereSubmesh.indexCount         = static_cast<UINT>(sphere.m_indices32.size());
+        sphereSubmesh.startIndexLocation = gridIndexOffset  + (UINT) grid.m_indices32.size();
+        sphereSubmesh.baseVertexLocation = gridVertexOffset + (UINT) grid.m_vertices.size();
+        vector<ShaderVertex> vertices(grid.m_vertices.size() + sphere.m_vertices.size());
         vector<uint32_t> indices;
-        for (size_t i = 0; i < grid.m_vertices.size(); ++i) {
-            vertices[i].pos = grid.m_vertices[i].m_position;
-            vertices[i].color = XMFLOAT4(DirectX::Colors::DarkGreen);
+        int k = 0;
+        for (size_t i = 0; i < grid.m_vertices.size(); ++i, k++) {
+            vertices[k].pos = grid.m_vertices[i].m_position;
+            vertices[k].color = XMFLOAT4(DirectX::Colors::DarkGreen);
+        }
+        for (size_t i = 0; i < sphere.m_vertices.size(); ++i, k++) {
+            vertices[k].pos = sphere.m_vertices[i].m_position;
+            vertices[k].color = XMFLOAT4(DirectX::Colors::DarkMagenta);
         }
         indices.insert(indices.end(), cbegin(grid.m_indices32), cend(grid.m_indices32));
-        const UINT vbNumBytes             = static_cast<UINT>(vertices.size()) * sizeof(ShaderVertex);
-        const UINT ibNumBytes             = static_cast<UINT>(indices.size()) * sizeof(uint32_t);
+        indices.insert(indices.end(), cbegin(sphere.m_indices32), cend(sphere.m_indices32));
+        const UINT vbNumBytes = static_cast<UINT>(vertices.size()) * sizeof(ShaderVertex);
+        const UINT ibNumBytes = static_cast<UINT>(indices.size()) * sizeof(uint32_t);
         unique_ptr<MeshGeometry> geometry = make_unique<MeshGeometry>();
-        geometry->name                    = "grid";
+        geometry->name = "all";
         ThrowIfFailed(D3DCreateBlob(vbNumBytes, &geometry->vertexBufferCPU));
         CopyMemory(geometry->vertexBufferCPU->GetBufferPointer(), vertices.data(), vbNumBytes);
         ThrowIfFailed(D3DCreateBlob(ibNumBytes, &geometry->indexBufferCPU));
         CopyMemory(geometry->indexBufferCPU->GetBufferPointer(), indices.data(), ibNumBytes);
-        geometry->vertexBufferGPU      = BaseUtil::CreateDefaultBuffer(m_d3dDevice.Get(), m_commandList.Get(), vertices.data(), vbNumBytes, geometry->vertexBufferUploader);
-        geometry->indexBufferGPU       = BaseUtil::CreateDefaultBuffer(m_d3dDevice.Get(), m_commandList.Get(), indices.data(), ibNumBytes, geometry->indexBufferUploader);
-        geometry->vertexByteStride     = sizeof(ShaderVertex);
+        geometry->vertexBufferGPU = BaseUtil::CreateDefaultBuffer(m_d3dDevice.Get(), m_commandList.Get(), vertices.data(), vbNumBytes, geometry->vertexBufferUploader);
+        geometry->indexBufferGPU = BaseUtil::CreateDefaultBuffer(m_d3dDevice.Get(), m_commandList.Get(), indices.data(), ibNumBytes, geometry->indexBufferUploader);
+        geometry->vertexByteStride = sizeof(ShaderVertex);
         geometry->vertexBufferByteSize = vbNumBytes;
-        geometry->indexFormat          = DXGI_FORMAT_R32_UINT;
-        geometry->indexBufferByteSize  = ibNumBytes;
-        geometry->drawArgs["grid"]     = gridSubmesh;
-        mGeometries[geometry->name]    = move(geometry);
+        geometry->indexFormat = DXGI_FORMAT_R32_UINT;
+        geometry->indexBufferByteSize = ibNumBytes;
+        geometry->drawArgs["grid"] = gridSubmesh;
+        geometry->drawArgs["sphere"] = sphereSubmesh;
+        mGeometries[geometry->name] = move(geometry);
     }
 
 private:
@@ -484,14 +530,15 @@ private:
     unique_ptr<UploadBuffer<SceneConstants>>        mSceneConstants = nullptr;
     ComPtr<ID3D12RootSignature>                     mRootSignature = nullptr;
     ComPtr<ID3D12PipelineState>                     mSimplePipeline = nullptr;
+    ComPtr<ID3D12PipelineState>                     mSkyRenderPipeline = nullptr;
     std::unique_ptr<Texture>                        mCubeMapTex;
-    float mRadius = 5.0f;
-    float mPhi = XM_PIDIV4;
-    float mTheta = 2.0f * XM_PI;
-    XMFLOAT4X4 mWorld = MathHelper::Identity4x4();
-    XMFLOAT4X4 mView = MathHelper::Identity4x4();
-    XMFLOAT4X4 mProj = MathHelper::Identity4x4();
-    POINT mLastMousePos;
+    float                                           mRadius = 5.0f;
+    float                                           mPhi = XM_PIDIV4;
+    float                                           mTheta = 2.0f * XM_PI;
+    XMFLOAT4X4                                      mWorld = MathHelper::Identity4x4();
+    XMFLOAT4X4                                      mView = MathHelper::Identity4x4();
+    XMFLOAT4X4                                      mProj = MathHelper::Identity4x4();
+    POINT                                           mLastMousePos;
 };
 
 // ======================================================================
